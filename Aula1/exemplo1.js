cb131bafa50e266a6aba3b1b616c3d8442bd0e04
joao marcelo <joaomarcelooliveira5000@gmail.com>
2026-02-25 15:00:52 -0300

    25/02

--- Aula1/exemplo1.js
+++ Aula1/exemplo1.js
@@ -9,5 +9,18 @@ document.write("nuemro 2 =" + numero2 + "<br>");
 document.write ("<h2>Operaçoes aritmeticas</h2>")
 var resultado;
 
-resultado = numer(numero1) + Number(numero2);
+resultado = Number(numero1) + Number(numero2);
+
 document.write("A soma dos numeros e" + resultado + "<br>");
+
+resultado = Number(numero1) - Number(numero2);
+
+document.write("A subtraçao do numero e" + resultado + "<br>")
+
+resultado = Number(numero1) * Number(numero2);
+
+document.write("O produtos do numero e" + resultado + "<br>")
+
+resultado = Number(numero1) / Number(numero2);
+
+document.write("A divisao entre os numero e" + resultado + "<br>")
---
+++ Aula1/exercicio1.js
@@ -0,0 +1,11 @@
+document.write("<h1>Exercicio 1</h1>");
+
+var numero = prompt("digite um numero qualquer: ")
+
+var quadrado = Number(numero) * Number(numero);
+var cubo = Number(numero) * Number(numero) * Number(numero)
+
+document.write(" <p> O numero digitado e " + numero+ "</p>")
+document.write(" <p> O quadrado do numero e " + quadrado+ "</p>")
+document.write(" <p> O cubo do numero e " + cubo+ "</p>")
+    
