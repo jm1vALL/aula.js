146cf1b92ff486793aaf7b121f93351978f33009
joao marcelo <joaomarcelooliveira5000@gmail.com>
2026-02-25 14:26:34 -0300

    jjk

---
+++ Aula1/exe1.script.js
@@ -0,0 +1,9 @@
+var nome = "joao marcelo"
+
+var idade = 17
+
+var altura = 1.76
+
+console.log(nome)
+console.log(idade)
+console.log(altura)
---
+++ Aula1/exemplo1.js
@@ -0,0 +1,13 @@
+document.write ("<h2> Entrada de dados</h2>");
+
+var numero1 = prompt("digite o numero 1");
+var numero2 = prompt("digite o numero 2");
+
+document.write("numero 1 =" + numero1 + "<Br>");
+document.write("nuemro 2 =" + numero2 + "<br>");
+
+document.write ("<h2>Operaçoes aritmeticas</h2>")
+var resultado;
+
+resultado = numer(numero1) + Number(numero2);
+document.write("A soma dos numeros e" + resultado + "<br>");
