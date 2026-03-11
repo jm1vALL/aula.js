8d18fa07328a7d7df48b37fcf714784fb7e1f094
joao marcelo <joaomarcelooliveira5000@gmail.com>
2026-03-11 15:00:13 -0300

    11/3

---
+++ Aula1/exercicio2.js
@@ -0,0 +1,12 @@
+document.write("<h1 style='color : red'>Aula 01 - Exercicio 1</h1>");
+
+var numero = prompt("Digite um numero qualquer: ");
+
+document.write("<p>O numero digitado foi " + numero +"</p>");
+
+var quadrado = Number(numero) * Number(numero);
+document.write("<p>O quadrado do numero digitado é" + quadrado +"</p>");
+
+var cubo = Number(numero) * Number(numero) * Number(numero);
+document.write("<p>O cubo do numero digitado é" + cubo + "</p>");
+
